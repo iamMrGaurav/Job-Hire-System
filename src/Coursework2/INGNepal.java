7c91737859e8f6e227f3ebc95b248f1460653ffe
Gaurav <gauravpaudel2013@gmail.com>
2020-05-05 14:14:56 +0530

    author added

--- src/Coursework2/INGNepal.java
+++ src/Coursework2/INGNepal.java
@@ -45,7 +45,7 @@ public class INGNepal implements ActionListener {
     JTextField JoiningDatef;
 
 
-    ArrayList<FullTimeStaffHire> lol;
+    ArrayList<StaffHire> staffHires;
 
     JFrame frame;
     JButton Add;
@@ -55,9 +55,10 @@ public class INGNepal implements ActionListener {
     JButton Terminate;
     JButton AddVaccancy;
 
-    public  INGNepal(){
+    JTextArea area;
 
-        lol = new ArrayList<FullTimeStaffHire>();
+    public  INGNepal(){
+        staffHires = new ArrayList<>();
     }
     public void layout(){
         frame=new JFrame("form");
@@ -67,7 +68,7 @@ public class INGNepal implements ActionListener {
 
         frame.setLayout(layout);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(700, 400);
         frame.setLocationRelativeTo(null);
 
         VaccancyNumber = new JLabel("Vaccancy Number:");
@@ -107,6 +108,9 @@ public class INGNepal implements ActionListener {
         WagesPerHoursf = new JTextField(10);
 
 
+         area  = new JTextArea();
+         area.setEditable(false);
+
         Add = new JButton("Add");
         Appoint = new JButton("Appoint");
         Display = new JButton("Display");
@@ -118,6 +122,7 @@ public class INGNepal implements ActionListener {
         gbc.gridx=0;
         gbc.gridy=0;
         gbc.gridwidth=1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         frame.add(StaffName,gbc);
 
         gbc.gridx=1;
@@ -298,9 +303,19 @@ public class INGNepal implements ActionListener {
         gbc.insets = new Insets(5,5,5,5);
         frame.add(Clear,gbc);
 
+        gbc.gridx=0;
+        gbc.gridy=9;
+        gbc.gridwidth=4;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        frame.add(area,gbc);
+
+
+
         AddVaccancy.addActionListener(this);
         Appoint.addActionListener(this);
         Terminate.addActionListener(this);
+        Display.addActionListener(this);
+        Clear.addActionListener(this);
 
 
         frame.setVisible(true);
@@ -308,27 +323,83 @@ public class INGNepal implements ActionListener {
 
     }
 
+    public int getVacancyNumber(){
+        return Integer.parseInt(VaccancyNumberf.getText());
+    }
+    public String getJobType(){
+        String jobType = "";
+        if(checkBoxPartTime.isSelected()){
+            jobType = "Part Time";
+        }else if(checkBoxFulTime.isSelected()){
+            jobType ="Full Time";
+        }
+        return jobType;
+    }
+
+    public int getWorkingHours(){
+        return Integer.parseInt(WorkingHoursf.getText());
+    }
+
+    public int getWagesPerHours(){
+        return Integer.parseInt(WagesPerHoursf.getText());
+    }
+
+    public String getDesignation(){
+        return (Designationf.getText());
+    }
+
+    public String getJoiningDate(){
+        return (JoiningDatef.getText());
+    }
+
+    public int getSalary(){
+        return Integer.parseInt(Salaryf.getText());
+    }
+
+    public String getStaffName(){
+        return (StaffNamef.getText());
+    }
+
+    public String getShifts(){
+        return WorkingShiftsf.getText();
+    }
+    public String getQualification(){
+        return Qualificationf.getText();
+    }
+
+    public String getAppointedBy(){
+        return (AppointedByf.getText());
+    }
+
+
+
+
     @Override
     public void actionPerformed(ActionEvent en){
         if (en.getSource() == AddVaccancy){
             try{
                 if (checkBoxFulTime.isSelected()){
-                    if(!VaccancyNumberf.equals("") && !Designationf.equals("") && !Salaryf.equals("") && !WorkingHoursf.equals("")){
-                        System.out.println(lol.size());
+                    if(!VaccancyNumberf.equals("") && !getDesignation().equals("") && !Salaryf.equals("") && !WorkingHoursf.equals("")){
+                        System.out.println(staffHires.size());
                         FullTimeStaffHire fullTimeStaffHire = new FullTimeStaffHire(Integer.parseInt(VaccancyNumberf.getText()), Designationf.getText(), labelFullTime.getText(), Integer.parseInt(Salaryf.getText()),  Integer.parseInt(WorkingHoursf.getText()));
-                        lol.add(Integer.parseInt(VaccancyNumberf.getText()), fullTimeStaffHire);
-                        System.out.println("Added successfully");
-                        System.out.println(lol.size());
+                        staffHires.add(Integer.parseInt(VaccancyNumberf.getText()), fullTimeStaffHire);
+                        clearFields();
                     } else {
                         JOptionPane.showMessageDialog(frame, "Empty fields detected");
                     }
                 }else if(checkBoxPartTime.isSelected()){
+                    if(!VaccancyNumberf.equals("") && !Designationf.equals("") && !WagesPerHoursf.equals("") && !WagesPerHoursf.equals("") && !WorkingShiftsf.equals("")){
+                        System.out.println(staffHires.size());
+                        PartTimeStaffHire partTimeStaffHire = new PartTimeStaffHire(Integer.parseInt(VaccancyNumberf.getText()),Designationf.getText(),labelPartTime.getText(),Integer.parseInt(WorkingHoursf.getText()),Integer.parseInt(WagesPerHoursf.getText()),WorkingShiftsf.getText());
+                        staffHires.add(Integer.parseInt(VaccancyNumberf.getText()),partTimeStaffHire);
+                        clearFields();
+                    }
                     System.out.println("Brother is selected");
                 }else{
                     JOptionPane.showMessageDialog(frame,"Please choose either full time or part time");
                 }
             }catch (IndexOutOfBoundsException ex){
-                JOptionPane.showMessageDialog(frame, "You cant use vacancy greater than " +lol.size()+ " at this time");
+                JOptionPane.showMessageDialog(frame, "You cant use vacancy greater than " +staffHires.size()+ " at this time");
             } catch (NumberFormatException ex){
                 JOptionPane.showMessageDialog(frame, "Please enter vacancy number, salary and working hour in number format.");
             }
@@ -336,9 +407,161 @@ public class INGNepal implements ActionListener {
             System.out.println("Appoint");
         }else if(en.getSource() == Terminate){
             System.out.println("Terminated");
+        }else if (en.getSource()==Display){
+            StringBuilder builder = new StringBuilder();
+            if(getJobType().equals("Full Time")){
+                for(StaffHire staffHire : staffHires){
+                    if (staffHire instanceof FullTimeStaffHire){
+                        FullTimeStaffHire fullTimeStaffHire = (FullTimeStaffHire) staffHire;
+                        fullTimeStaffHire.display();
+//                        builder.append("Vacancy Number");
+//                        builder.append(" | ");
+//                        builder.append("Designation");
+//                        builder.append(" | ");
+//                        builder.append("Job Type\n");
+
+                        builder.append(staffHire.getVacancyNumber());
+                        builder.append(" | ");
+                        builder.append(staffHire.getDesignation());
+                        builder.append(" | ");
+                        builder.append(staffHire.getJobType());
+                        builder.append(" | ");
+                        if(fullTimeStaffHire.getJoined()){
+                            builder.append(((FullTimeStaffHire) staffHire).getQualification());
+                            builder.append(" | ");
+                            builder.append(((FullTimeStaffHire) staffHire).getStaffName());
+                            builder.append(" | ");
+                            builder.append(((FullTimeStaffHire) staffHire).getAppointedBy());
+                            builder.append(" | ");
+                            builder.append(((FullTimeStaffHire) staffHire).getJoiningDate());
+                            builder.append(" | ");
+                        }
+                    }
+                }
+                area.setText(String.valueOf(builder));
+            }else  if (getJobType().equals("Part Time")){
+
+            }else {
+                JOptionPane.showMessageDialog(frame, "Please Select job type.");
+            }
+//            for (StaffHire staffHire:staffHires){
+//                if(staffHire instanceof  FullTimeStaffHire) {
+//                    FullTimeStaffHire fullTimeStaffHire =(FullTimeStaffHire) staffHire;
+//
+//                    builder.append(fullTimeStaffHire.getStaffName());
+//                    if(!getStaffName().isEmpty()){
+//                        builder.append(" ");
+//                    }
+//
+//                    builder.append(fullTimeStaffHire.getDesignation());
+//                    if(!getDesignation().isEmpty()){
+//                        builder.append(" ");
+//                    }
+//
+//                    builder.append(fullTimeStaffHire.getJoiningDate());
+//                    if(!fullTimeStaffHire.getJoiningDate().isEmpty()){
+//                        builder.append(" ");
+//                    }
+//
+//                    builder.append(fullTimeStaffHire.getSalary());
+//                    if(fullTimeStaffHire.getSalary() != 0){
+//                        builder.append(" ");
+//                    }
+//
+//                    builder.append(fullTimeStaffHire.getWorkingHour());
+////                    if(!fullTimeStaffHire.getWorkingHour().isEmpty()){
+////                        builder.append(",");
+////                    }
+//
+//
+//                    builder.append(fullTimeStaffHire.getAppointedBy());
+//                    if(!fullTimeStaffHire.getAppointedBy().isEmpty()){
+//                        builder.append(",");
+//                    }
+//
+//                    builder.append(fullTimeStaffHire.getVacancyNumber());
+//                    builder.append(",");
+//
+//                    builder.append(fullTimeStaffHire.getJobType());
+//                    if(!fullTimeStaffHire.getJobType().isEmpty()){
+//                        builder.append(",");
+//                    }
+//
+//
+//
+//                }
+//                if (builder.toString().isEmpty()){
+//                    area.setText(" Data not available!");
+//
+//                }else {
+//                    area.setText(builder.toString());
+//                } if (staffHire instanceof PartTimeStaffHire){
+//                    PartTimeStaffHire partTimeStaffHire = (PartTimeStaffHire)staffHire;
+//
+//                    builder.append(partTimeStaffHire.getStaffName());
+////                    if(!PartTimeStaffHire.getStaffName().isEmpty()){
+////                        builder.append(",");
+////                    }
+//
+//                    builder.append(partTimeStaffHire.getVacancyNumber());
+//                    builder.append(",");
+//
+//                    builder.append(partTimeStaffHire.getJobType());
+//                    if(!partTimeStaffHire.getJobType().isEmpty()){
+//                        builder.append(",");
+//                    }
+//
+//                    builder.append(partTimeStaffHire.getWagesPerHour());
+//                    builder.append(",");
+//
+//                    builder.append(partTimeStaffHire.getWorkingHour());
+//                    builder.append(",");
+//
+//                    builder.append(partTimeStaffHire.getAppointedBy());
+//                    if(!partTimeStaffHire.getAppointedBy().isEmpty()){
+//                        builder.append(",");
+//                    }
+//
+//                    builder.append(partTimeStaffHire.getDesignation());
+//                    if(!partTimeStaffHire.getDesignation().isEmpty()){
+//                        builder.append(",");
+//                    }
+//
+////                    if(!PartTimeStaffHire.getShifts().isEmpty()){
+////                        builder.append(",");
+////                    }
+//
+//
+//
+//
+//                }
+//
+//
+//            }
+
         }
+        else if (en.getSource()==Clear){
+            clearFields();
+        }
+
     }
 
+    public void clearFields(){
+        VaccancyNumberf.setText("");
+        Designationf.setText("");
+        Salaryf.setText("");
+        WorkingHoursf.setText("");
+        StaffNamef.setText("");
+        Qualificationf.setText("");
+        JoiningDatef.setText("");
+        AppointedByf.setText("");
+        WorkingShiftsf.setText("");
+        WagesPerHoursf.setText("");
+        area.setText("");
+        checkBoxFulTime.setSelected(false);
+        checkBoxPartTime.setSelected(false);
+
+    }
     public static void main(String[] args) {
         new INGNepal().layout();
         System.out.println("hi");
